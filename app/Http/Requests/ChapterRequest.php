c32d936d351bb5c368a5e4d7b50ae277b575dd10
Naresh khuriwal <naresh.khuriwal89@gmail.com>
2026-02-05 16:17:55 +0530

    update api for chapter

--- app/Http/Requests/ChapterRequest.php
+++ app/Http/Requests/ChapterRequest.php
@@ -12,6 +12,7 @@ class ChapterRequest extends FormRequest
         return [
             'title'         => ['required','string','max:255'],
             'chapter_type'         => ['required','string','max:255'],
+            'chapter_section' => 'required|string|max:255',
             'collection_id' => ['nullable','exists:collections,id'],
             'order_index'   => ['nullable','integer'],
             'body_html'     => ['nullable','string'],
--- app/Models/Chapter.php
+++ app/Models/Chapter.php
@@ -6,7 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class Chapter extends Model
 {
-    protected $fillable = ['user_id','collection_id','title','order_index', 'body_html', 'chapter_type'];
+    protected $fillable = ['user_id','collection_id','title','order_index', 'body_html', 'chapter_type', 'chapter_section'];
+
 
     public function user() { return $this->belongsTo(User::class); }
     public function collection() { return $this->belongsTo(Collection::class); }
